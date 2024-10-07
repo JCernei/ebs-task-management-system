21575784f8bfa81108616a22639a5455d88d0f7e
JCernei <jonnycernei@gmail.com>
2024-10-08 01:51:26 +0300

    Remove url trailing slash

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from apps.users.views import RegisterUserView, LoginView, UserListView
 
 urlpatterns = [
     path("register", RegisterUserView.as_view(), name="token_register"),
-    path("login/", LoginView.as_view(), name="login_user"),
+    path("login", LoginView.as_view(), name="login_user"),
     path("", UserListView.as_view(), name="user_list"),
     path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
