ef0d1d05ce773e8af4fc32ac2f56a9842f4c487b
JCernei <jonnycernei@gmail.com>
2024-10-08 11:47:20 +0100

    Add filtering for tasks based on completion

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-# from . import views
-from .views import TaskListCreateView, TaskDetailView, UserTasksView
+from .views import TaskListCreateView, TaskDetailView, UserTaskListView
+
 
 urlpatterns = [
     path("tasks", TaskListCreateView.as_view(), name="task_list_create"),
     path('tasks/<int:pk>', TaskDetailView.as_view(), name='task_detail'),
-    path('users/<int:pk>/tasks', UserTasksView.as_view(), name='user_tasks'),
+    path('users/<int:pk>/tasks', UserTaskListView.as_view(), name='user_task_list'),
 ]
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -3,8 +3,21 @@ from rest_framework.permissions import IsAuthenticated
 from apps.tasks.serializers import TaskSerializer, TaskDetailSerializer, SimpleTaskSerializer
 from .models import Task
 from django.contrib.auth.models import User
+from drf_spectacular.utils import OpenApiParameter, extend_schema_view, extend_schema
+from drf_spectacular.types import OpenApiTypes
 
-
+@extend_schema_view(
+    get=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'completed',
+                type=OpenApiTypes.BOOL,
+                location=OpenApiParameter.QUERY,
+                description='Filter tasks by completion status (true or false)',
+            ),
+        ]
+    )
+)
 class TaskListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
@@ -13,9 +26,16 @@ class TaskListCreateView(ListCreateAPIView):
             return SimpleTaskSerializer
         return TaskSerializer
 
-    # Define queryset for GET requests (list tasks)
+
     def get_queryset(self):
-        return Task.objects.all()
+        queryset = Task.objects.all()
+        completed = self.request.query_params.get('completed', None)
+
+        if completed is not None:
+            completed = completed.lower() == 'true'
+            queryset = queryset.filter(is_completed=completed)
+
+        return queryset
 
     # Define behavior for POST requests (create task)
     def perform_create(self, serializer):
@@ -25,15 +45,33 @@ class TaskListCreateView(ListCreateAPIView):
         )
 
 
-class UserTasksView(ListAPIView):
+@extend_schema_view(
+    get=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'completed',
+                type=OpenApiTypes.BOOL,
+                location=OpenApiParameter.QUERY,
+                description='Filter tasks by completion status (true or false)',
+            ),
+        ]
+    )
+)
+class UserTaskListView(ListAPIView):
     serializer_class = SimpleTaskSerializer
     permission_classes = [IsAuthenticated]
 
-    # Filter tasks to only return those assigned to the user in the URL
     def get_queryset(self):
         user_id = self.kwargs['pk']
         user = get_object_or_404(User, pk=user_id)
-        return Task.objects.filter(assigned_to=user)
+        queryset = Task.objects.filter(assigned_to=user)
+
+        completed = self.request.query_params.get('completed', None)
+        if completed is not None:
+            completed = completed.lower() == 'true'
+            queryset = queryset.filter(is_completed=completed)
+
+        return queryset
 
 
 class TaskDetailView(RetrieveAPIView):
