5b7f87e4984edac3e5f2eedcd0281a70a8364910
JCernei <jonnycernei@gmail.com>
2024-10-10 16:57:10 +0300

    Add comment creation and listing

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,10 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from apps.tasks.models import Task
+from apps.tasks.models import Task, Comment
 from apps.tasks.serializers import TaskSerializer, TaskDetailSerializer, TaskListSerializer, \
-    TaskUpdateSerializer, TaskCreateSerializer
+    TaskUpdateSerializer, TaskCreateSerializer, CommentCreateSerializer, CommentListSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -22,4 +24,24 @@ class TaskViewSet(viewsets.ModelViewSet):
             return TaskCreateSerializer
         elif self.action == 'partial_update':
             return TaskUpdateSerializer
+        elif self.action == 'comments' and self.request.method == 'POST':
+            return CommentCreateSerializer
+        elif self.action == 'comments' and self.request.method == 'GET':
+            return CommentListSerializer
         return TaskSerializer
+
+    @action(detail=True, methods=['post', 'get'], url_path='comments', url_name='task_comments')
+    def comments(self, request, pk=None):
+        task = self.get_object()
+
+        if request.method == 'POST':
+            serializer = self.get_serializer(data=request.data, context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            validated_data = serializer.validated_data
+            serializer.save(task=task, **validated_data)  # Set task and user
+            return Response(serializer.data, status=201)
+
+        if request.method == 'GET':
+            comments = Comment.objects.filter(task=task)
+            serializer = self.get_serializer(comments, many=True)
+            return Response(serializer.data)
