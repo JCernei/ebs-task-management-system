0c3c3baa58fd2d6a9f975d5ec26562250060a10e
JCernei <jonnycernei@gmail.com>
2024-10-10 22:39:01 +0300

    Refactor task comment actions

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -24,24 +24,24 @@ class TaskViewSet(viewsets.ModelViewSet):
             return TaskCreateSerializer
         elif self.action == 'partial_update':
             return TaskUpdateSerializer
-        elif self.action == 'comments' and self.request.method == 'POST':
-            return CommentCreateSerializer
-        elif self.action == 'comments' and self.request.method == 'GET':
+        elif self.action == 'list_comment':
             return CommentListSerializer
+        elif self.action == 'create_comment':
+            return CommentCreateSerializer
         return TaskSerializer
 
-    @action(detail=True, methods=['post', 'get'], url_path='comments', url_name='task_comments')
-    def comments(self, request, pk=None):
+    @action(detail=True, url_path='comments', url_name='task_comments')
+    def list_comment(self, request, pk=None):
         task = self.get_object()
+        comments = Comment.objects.filter(task=task)
+        serializer = self.get_serializer(comments, many=True)
+        return Response(serializer.data)
 
-        if request.method == 'POST':
-            serializer = self.get_serializer(data=request.data, context={'request': request})
-            serializer.is_valid(raise_exception=True)
-            validated_data = serializer.validated_data
-            serializer.save(task=task, **validated_data)  # Set task and user
-            return Response(serializer.data, status=201)
-
-        if request.method == 'GET':
-            comments = Comment.objects.filter(task=task)
-            serializer = self.get_serializer(comments, many=True)
-            return Response(serializer.data)
+    @list_comment.mapping.post
+    def create_comment(self, request, pk=None):
+        task = self.get_object()
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        serializer.save(task=task, **validated_data)  # Set task and user
+        return Response(serializer.data, status=201)
