9f48793dd5523b0922213bc06a0e30aed106a260
JCernei <jonnycernei@gmail.com>
2024-10-24 14:49:04 +0300

    Fix user delete behaviour on tasks and comments

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -7,15 +7,15 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     is_completed = models.BooleanField(default=False)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task_owner', null=True, editable=False)
-    executor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task_executor', null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='task_owner', null=True, editable=False)
+    executor = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='task_executor', null=True)
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     task = models.ForeignKey('Task', on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
