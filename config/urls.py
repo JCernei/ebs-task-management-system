7bf26c6fbe536b73d7b602c19867afe32dd418f7
JCernei <jonnycernei@gmail.com>
2024-11-07 10:31:06 +0200

    Add authentication with github

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
-
 urlpatterns = [
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("schema", SpectacularAPIView.as_view(), name="schema"),
